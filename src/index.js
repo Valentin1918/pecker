35f8aff5cd2ead06d04e3dc850c376daf0ec021e
Grekulov <grekulov.valentyn@gmail.com>
2019-04-26 15:53:46 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -7,11 +7,13 @@ export const pecker = {
       } catch (e) {
         console.log('Some error occurred: ', e);
     }}, delay);
-    setTimeout(() => this.clear(dieCb), dieDelay);
+    this.timeout = setTimeout(() => this.clear(dieCb), dieDelay);
   },
   clear: function (dieCb) {
     if (this.interval) clearInterval(this.interval);
+    if (this.timeout) clearTimeout(this.timeout);
     this.interval = null;
+    this.timeout = null;
     if (typeof dieCb === 'function') dieCb();
   }
 };
